8a1f7e2f4e6b4c64681f82748b71ba41e2b212cf
Daniel Ojeda <211@holbertonschool.com>
2018-03-30 03:11:16 +0000

    fix

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -2,10 +2,10 @@
 
 
 file { 'holberton':
-     ensure  => file,
-     path    => '/tmp/holberton',
-     mode    => '0744',
-     owner   => 'www-data',
-     group   => 'www-data',
-     content => 'I love Puppet',     
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',     
 }
