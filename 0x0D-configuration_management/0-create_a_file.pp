d52d7b4234dd1ea3cda0e9386768220cdd97bc05
Daniel Ojeda <211@holbertonschool.com>
2018-03-30 03:24:08 +0000

    puppet-lint conforming

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,11 +1,9 @@
 # create a file in /tmp using puppet
-
-
 file { '/tmp/holberton':
   ensure  => file,
   path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',     
+  content => 'I love Puppet',
 }
