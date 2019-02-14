969b0e0c79b0d43c12cf7acf7e51a19ce7a66846
Daniel Ojeda <211@holbertonschool.com>
2019-02-14 02:17:30 +0000

    get subreddit for category

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+0-subs module
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    number of subscribers
+    """
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    response = requests.get('https://www.reddit.com/r/{}/about.json'.
+                            format(subreddit),
+                            headers={'User-Agent': 'Daniel:Api:Project'})
+    if response.status_code != 200:
+        return 0
+    response_dict = response.json()
+    return response_dict['data']['subscribers']
